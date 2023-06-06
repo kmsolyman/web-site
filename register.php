d10342611162259b0abbb07a1ce25f823fa8f233
kmsolyman <134826118+kmsolyman@users.noreply.github.com>
2023-06-06 17:35:26 +0600

    Update register.php
    Ok

--- register.php
+++ register.php
@@ -83,11 +83,11 @@ form, .content {
   	<?php include('errors.php'); ?>
   	<div class="input-group">
   	  <label>Username</label>
-  	  <input type="text" name="username" value="<?php echo $username; ?>">
+  	  <input type="text" name="username" value="<?php echo $username; ?">
   	</div>
   	<div class="input-group">
   	  <label>Email</label>
-  	  <input type="email" name="email" value="<?php echo $email; ?>">
+  	  <input type="email" name="email" value="<?php echo $email; ?">
   	</div>
   	<div class="input-group">
   	  <label>Password</label>
