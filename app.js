51d090136a584960cb74564222fb20ba3a39cdd0
kmsolyman <134826118+kmsolyman@users.noreply.github.com>
2023-06-07 10:55:57 +0600

    Update app.js
    ok

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const mysql = require('mysql')
 
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
